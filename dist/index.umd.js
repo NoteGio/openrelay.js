f341895fb21b8724a1ff0ef94eec223831a5e54e
Austin Roberts <code@ausiv.com>
2017-11-28 11:30:24 -0600

    Get rid of debug logging

--- dist/index.umd.js
+++ dist/index.umd.js
@@ -705,28 +705,18 @@ var OrderFilter = function () {
         for (var _iterator = _getIterator(this.orders), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
           var order = _step.value;
 
-          console.log(_JSON$stringify(order));
           var match = true;
           for (var key in parameters) {
-            console.log(key);
             if (key == "trader") {
               if (!(order["maker"] == parameters[key] || order["taker"] == parameters[key])) {
-                console.log(key);
-                console.log(order.maker, parameters[key]);
-                console.log(order.taker, parameters[key]);
                 match = false;
               }
             } else if (key == "tokenAddress") {
               if (!(order["makerTokenAddress"] == parameters[key] || order["takerTokenAddress"] == parameters[key])) {
-                console.log(key);
-                console.log(order.makerTokenAddress, parameters[key]);
-                console.log(order.takerTokenAddress, parameters[key]);
                 match = false;
               }
             } else {
               if (order[key] != parameters[key]) {
-                console.log(key);
-                console.log(order[key], parameters[key]);
                 match = false;
               }
             }
--- src/OrderFilter.js
+++ src/OrderFilter.js
@@ -5,28 +5,18 @@ export class OrderFilter {
   search(parameters) {
     var results = [];
     for(var order of this.orders) {
-      console.log(JSON.stringify(order));
       var match = true;
       for(var key in parameters) {
-        console.log(key);
         if(key == "trader") {
           if(!(order["maker"] == parameters[key] || order["taker"] == parameters[key])) {
-            console.log(key);
-            console.log(order.maker, parameters[key]);
-            console.log(order.taker, parameters[key]);
             match = false;
           }
         } else if (key == "tokenAddress") {
           if(!(order["makerTokenAddress"] == parameters[key] || order["takerTokenAddress"] == parameters[key])) {
-            console.log(key);
-            console.log(order.makerTokenAddress, parameters[key]);
-            console.log(order.takerTokenAddress, parameters[key]);
             match = false;
           }
         } else {
           if(order[key] != parameters[key]) {
-            console.log(key);
-            console.log(order[key], parameters[key]);
             match = false;
           }
         }
